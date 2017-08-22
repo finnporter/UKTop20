b7945c21112e1b8260cc23f6eca584e847ca110e
finnporter <iamfinnporter@gmail.com>
2017-08-22 17:48:58 +0100

    creates MainContainer and displays h1

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainContainer from './containers/MainContainer.jsx';
+
+window.onload = function(){
+  ReactDOM.render(
+    <MainContainer />,
+    document.getElementById('app')
+  );
+}
---
+++ client/src/containers/MainContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>UK Top 20</h1>
+      </div>
+    )
+  }
+}
+
+export default MainContainer;
